8e6358131d786b575eb5bf57c94f1ce8556ba33f
Dursun Köktürk <dursunserefkokturk@gmail.com>
2022-09-09 10:14:02 +0300

    Encapsulation And Getter Setter

---
+++ src/day2_homework_30/Main.java
@@ -0,0 +1,22 @@
+package day2_homework_30;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Getter Ve Setter Ile Calismak
+		
+		// Kisitmala Yada Yetkilendirme Durumlarinda 
+		// Kimlere Izin Verilmis Ise Veriyi Sadece Onlar Okuyabilir Ve Yazabilir 
+		Product product = new Product();
+		product.setId(1);
+		product.setName("Laptop");
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
+		
+		ProductManager productManager = new ProductManager();
+		productManager.Add(product);
+		System.out.println(product.getProductCode());
+	}
+}
---
+++ src/day2_homework_30/ProductManager.java
@@ -0,0 +1,7 @@
+package day2_homework_30;
+
+public class ProductManager {
+	public void Add(Product product) {
+		System.out.println("Product Added : " + product.getName());
+	}
+}
